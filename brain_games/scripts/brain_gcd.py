4ed83fa02bb7caf149eaab0a1ec3c83c1c48aa5d
maltush <maltushw@gmail.com>
2025-04-22 09:53:45 +0300

    Исправление ошибки в gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,6 +1,23 @@
-from engine.game_manager import GameManager
-from games.gcd_game import play_game
+import random
+import math
 
-if __name__ == "__main__":
-    manager = GameManager()
-    manager.start_game(play_game)
+def find_gcd(a, b):
+    return math.gcd(a, b)
+
+def play_game():
+    print('Find the greatest common divisor of given numbers.')
+
+    for _ in range(3):  
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        correct_answer = find_gcd(num1, num2)
+
+        print(f"Question: {num1} {num2}")
+        user_answer = input('Your answer: ')
+
+        if int(user_answer) != correct_answer:
+            return False, correct_answer
+        
+        print("Correct!")
+
+    return True, None
