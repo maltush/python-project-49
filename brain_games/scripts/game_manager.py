abfab628b4b4ef1cd0260f5c077caed4d26f9624
maltush <maltushw@gmail.com>
2025-05-23 14:04:59 +0300

    Исправление точек входа

--- brain_games/scripts/game_manager.py
+++ brain_games/scripts/game_manager.py
@@ -1,13 +1,15 @@
 max_rounds = 3
 
-def start_game(play_game):
-        name = input("May I have your name? ")
-        print(f"Hello, {name}!")
 
-        success, correct_answer = play_game()
-        
-        if not success:
-            print(f"'{correct_answer}' is the correct answer.")
-            print(f"Let's try again, {name}!")
-        else:
-            print(f"Congratulations, {name}!")
+def start_game(play_game):
+    
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
+    success, correct_answer = play_game()
+    
+    if not success:
+        print(f"'{correct_answer}' is the correct answer.")
+        print(f"Let's try again, {name}!")
+    else:
+        print(f"Congratulations, {name}!")
